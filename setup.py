89fa38f9f8fe5ba3ab7927da7ee7e32ae1c0bac1
aws-kh <76266750+aws-kh@users.noreply.github.com>
2021-06-11 01:29:09 +0000

    FIX: updated setupy.py to require numpy>=1.20.0 for sliding_window_view

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     install_requires=[
         "statsmodels>=0.12.2",
         "pandas>=1.2.4",
-        "numpy",
+        "numpy>=1.20.0",
         "scipy>=1.6.0",
         "tqdm"
     ],
