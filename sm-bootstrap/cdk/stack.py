232231e6b2fcdf30c67faf900286a54ac0a869a3
aws-kh <aws-kh@users.noreply.github.com>
2021-07-20 11:04:09 +0000

    Added SNS email notification and landing page.

--- sm-bootstrap/cdk/stack.py
+++ sm-bootstrap/cdk/stack.py
@@ -1,32 +1,37 @@
 from aws_cdk import core as cdk
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import (
     aws_ec2 as ec2,
-#   aws_ecs as ecs,
-#   aws_ecr as ecr,
-#   aws_iam as iam,
-#   aws_cognito as cognito,
-#   aws_ecs_patterns as ecs_patterns,
-#   aws_elasticloadbalancingv2 as elb,
-#   aws_elasticloadbalancingv2_actions as elb_actions,
-#   aws_certificatemanager as certificatemanager,
     aws_iam as iam,
     aws_sagemaker as sm,
     aws_sns as sns,
-    aws
+    aws_sns_subscriptions as subscriptions,
+    aws_lambda as lambda_,
     core
 )
 
 #
 # Lifecycle config raw strings
 #
-LCC_ONSTART_STR = r"""#!/bin/bash
+
+# This is run *each time* the notebook instance is started
+LCC_ONSTART_STR = """#!/bin/bash
 set -e
 initctl restart jupyter-server --no-wait
+
+# Get the notebook URL
+DASHBOARD_URL=https://$(aws sagemaker describe-notebook-instance \
+    --notebook-instance-name {notebook_instance_name} \
+    --query "Url" \
+    --output text)/proxy/8501/
+
+# Get the instructions ipynb notebook URL (email to user)
+LANDING_PAGE_URL=https://$DASHBOARD_URL/lab/tree/SFS_Landing_Page.ipynb
+
+# Send SNS email
+aws lambda invoke --function-name {sns_lambda_function} \
+    --payload '{{"landing_page_url": "'$LANDING_PAGE_URL'", "dashboard_url": "'$DASHBOARD_URL'"}}' \
+    /dev/stdout
 
 #
 # Start SFS dashboard in the background
@@ -35,7 +40,8 @@ source /home/ec2-user/anaconda3/bin/activate sfs
 streamlit hello &
 """
 
-LCC_ONCREATE_STR = r"""#!/bin/bash
+# This is run *once* ever, upon the *creation* of the notebook
+LCC_ONCREATE_STR = """#!/bin/bash
 set -e
 export LC_ALL=en_US.utf-8 && export LANG=en_US.utf-8
 source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
@@ -64,18 +70,9 @@ curl -sL https://rpm.nodesource.com/setup_14.x | bash - \
 cd ./lambdamap_cdk/
 pip install -q -r ./requirements.txt
 cdk deploy --require-approval never \
-    --context stack_name=SfsLambdaMapStack \
+    --context stack_name={sfs_lambdamap_stack_name} \
     --context function_name=SfsLambdaMapFunction \
     --context extra_cmds='pip install -q git+https://github.com/aws-samples/simple-forecast-solution.git#egg=sfs'
-
-# Get the notebook URL
-NOTEBOOK_URL=$(aws sagemaker describe-notebook-instance \
-    --notebook-instance-name {construct_id}-NotebookInstance \
-    --query "Url" \
-    --output text)
-
-# Get the instructions ipynb notebook URL (email to user)
-INSTRUCTIONS_URL=$NOTEBOOK_URL/lab/tree/Instructions.ipynb
 
 #
 # Upgrade jupyter-server-proxy
@@ -84,6 +81,44 @@ source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
 
 pip uninstall -q --yes nbserverproxy || true
 pip install -q --upgrade jupyter-server-proxy
+
+su - ec2-user
+mkdir -p /home/ec2-user/SageMaker/output/
+"""
+
+# This is the lambda that sends the notification email to the user once
+# the dashboard is deployed, it contains the URL to the landing page
+# sagemaker notebook.
+SNS_EMAIL_LAMBDA_INLINE = """import os
+import json
+import boto3
+import textwrap
+
+def lambda_handler(event, context):
+    landing_page_url = event["landing_page_url"]
+    dashboard_url = event["dashboard_url"]
+
+    client = boto3.client("sns")
+    response = client.publish(
+        TopicArn=os.environ["TOPIC_ARN"],
+        Subject="Your Amazon SFS Dashboard is Ready!",
+        Message=textwrap.dedent(f'''
+        Congratulations!
+        
+        Amazon SFS has been successfully deployed into your AWS account.
+        
+        Your Amazon SFS landing page is available here:
+        ‣ {landing_page_url}
+        
+        You Amazon SFS dashboard is available here:
+        ‣ {dashboard_url}
+
+        Sincerely,
+        The Amazon SFS Team
+        ‣ github: https://github.com/aws-samples/simple-forecast-solution
+        '''))
+    
+    return response
 """
 
 
@@ -91,18 +126,50 @@ class BootstrapStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        email_address = core.CfnParameter(self, "emailAddress")
+        instance_type = core.CfnParameter(self, "instanceType")
+
+        #
+        # SNS topic for email notification
+        #
+        topic = sns.Topic(self, f"{construct_id}-NotificationTopic",
+                topic_name=f"{construct_id}-NotificationTopic")
+
+        topic.add_subscription(
+            subscriptions.EmailSubscription(email_address.value_as_string))
+
+        sns_lambda_role = iam.Role(
+            self,
+            f"{construct_id}-SnsEmailLambdaRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSNSFullAccess")
+            ])
+
+        sns_lambda = lambda_.Function(self, f"{construct_id}-SnsEmailLambda",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            environment={"TOPIC_ARN": f"arn:aws:sns:{self.region}:{self.account}:{topic.topic_name}"},
+            code=lambda_.Code.from_inline(SNS_EMAIL_LAMBDA_INLINE),
+            handler="index.lambda_handler",
+            role=sns_lambda_role)
+
         #
         # Notebook lifecycle configuration
         #
+        notebook_instance_name=f"{construct_id}-NotebookInstance"
+
         lcc_onstart_obj = \
             sm.CfnNotebookInstanceLifecycleConfig \
               .NotebookInstanceLifecycleHookProperty(
-                  content=core.Fn.base64(LCC_ONSTART_STR))
+                  content=core.Fn.base64(LCC_ONSTART_STR.format(
+                      notebook_instance_name=notebook_instance_name,
+                      sns_lambda_function=sns_lambda.function_name)))
 
         lcc_oncreate_obj = \
             sm.CfnNotebookInstanceLifecycleConfig \
               .NotebookInstanceLifecycleHookProperty(
-                  content=core.Fn.base64(LCC_ONCREATE_STR.format(construct_id=construct_id)))
+                  content=core.Fn.base64(LCC_ONCREATE_STR.format(
+                      sfs_lambdamap_stack_name=f"{construct_id}-SfsLambdaMapStack")))
 
         lcc = sm.CfnNotebookInstanceLifecycleConfig(
             self,
@@ -133,5 +200,5 @@ class BootstrapStack(cdk.Stack):
             f"{construct_id}-NotebookInstance",
             role_arn=sm_role.role_arn,
             instance_type="ml.t3.large",
-            notebook_instance_name=f"{construct_id}-NotebookInstance",
+            notebook_instance_name=notebook_instance_name,
             lifecycle_config_name=lcc.attr_notebook_instance_lifecycle_config_name)
