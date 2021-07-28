e0d28fbc44832fc680dc0c3a004e53217a9ce906
aws-kh <76266750+aws-kh@users.noreply.github.com>
2021-07-28 22:56:15 +0000

    Merged afc_stack.py into stack.py

--- sm-bootstrap/cdk/stack.py
+++ sm-bootstrap/cdk/stack.py
@@ -1,3 +1,5 @@
+import os
+
 from aws_cdk import core as cdk
 
 from aws_cdk import (
@@ -8,8 +10,51 @@ from aws_cdk import (
     aws_sns as sns,
     aws_sns_subscriptions as subscriptions,
     aws_lambda as lambda_,
+    aws_ec2 as ec2,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
     core
 )
+
+PWD = os.path.dirname(os.path.realpath(__file__))
+
+# This is the lambda that sends the notification email to the user once
+# the dashboard is deployed, it contains the URL to the landing page
+# sagemaker notebook.
+SNS_EMAIL_LAMBDA_INLINE = """import os
+import re
+import json
+import boto3
+import textwrap
+
+def lambda_handler(event, context):
+    payload = event["input"]["Payload"]
+
+    # get s3 location of the exports
+    afc_client = boto3.client("forecast")
+    resp = afc_client.describe_forecast_export_job(
+        ForecastExportJobArn=payload["ForecastExportJobArn"])
+    console_url = payload["AwsS3ConsoleUrl"]
+
+    client = boto3.client("sns")
+
+    response = client.publish(
+        TopicArn=os.environ["TOPIC_ARN"],
+        Subject="[Amazon SFA] Your Amazon Forecast job has completed!",
+        Message=textwrap.dedent(f'''
+        Hi!
+
+        Your Amazon Forecast job has completed.
+
+        The forecast files can be downloaded from the S3 path below:
+        ‣ {console_url}
+
+        Sincerely,
+        The Amazon SFS Team
+        ‣ https://github.com/aws-samples/simple-forecast-solution
+        '''))
+    return response
+"""
 
 #
 # Lifecycle config raw strings
@@ -44,6 +89,7 @@ aws lambda invoke --function-name {sns_lambda_function} \
 
 # Update the url in the landing page
 sed -i 's|INSERT_URL_HERE|'$LANDING_PAGE_URL'|' /home/ec2-user/SageMaker/SFS_Landing_Page.ipynb
+mkdir -p /home/ec2-user/SageMaker/reports/
 
 #
 # Start SFS dashboard in the background
@@ -260,3 +306,287 @@ class SfsStack(cdk.Stack):
             instance_type=instance_type.value_as_string,
             notebook_instance_name=notebook_instance_name,
             lifecycle_config_name=lcc.attr_notebook_instance_lifecycle_config_name)
+
+        #
+        # AFC/Lambda role
+        #
+        afc_role = iam.Role(
+            self,
+            f"{construct_id}-AfcRole",
+            role_name=f"AfcRole",
+            assumed_by=iam.CompositePrincipal(
+                iam.ServicePrincipal("forecast.amazonaws.com"),
+                iam.ServicePrincipal("lambda.amazonaws.com")
+            ),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonForecastFullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSLambda_FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchLogsFullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSNSFullAccess")
+            ])
+
+        fail_state = sfn.Fail(self, "Fail")
+        succeed_state = sfn.Succeed(self, "Succeed")
+
+        #
+        # PREPARE DATA
+        #
+        prepare_lambda = \
+            lambda_.Function(
+                self,
+                "PrepareLambda",
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                handler="index.prepare_handler",
+                code=lambda_.Code.from_asset(os.path.join(PWD, "afc_lambdas")),
+                environment={
+                        "AFC_ROLE_ARN": afc_role.role_arn
+                    },
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+
+        prepare_step = \
+            tasks.LambdaInvoke(
+                self,
+                "PrepareDataStep",
+                lambda_function=prepare_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        #
+        # CREATE PREDICTOR
+        #
+        create_predictor_lambda = \
+            lambda_.Function(
+                self,
+                "CreatedPredictorLambda",
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                handler="index.create_predictor_handler",
+                code=lambda_.Code.from_asset(os.path.join(PWD, "afc_lambdas")),
+                environment={
+                    "AFC_ROLE_ARN": afc_role.role_arn
+                },
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+
+        create_predictor_step = \
+            tasks.LambdaInvoke(
+                self,
+                "CreatePredictorStep",
+                lambda_function=create_predictor_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        create_predictor_step.add_retry(
+                backoff_rate=1.05,
+                interval=core.Duration.seconds(60),
+                max_attempts=1000,
+                errors=["ResourceNotFoundException",
+                        "ResourceInUseException",
+                        "ResourcePendingException"])
+
+        #
+        # CREATE FORECAST
+        #
+        create_forecast_lambda = \
+            lambda_.Function(
+                self,
+                "CreatedforecastLambda",
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                handler="index.create_forecast_handler",
+                code=lambda_.Code.from_asset(os.path.join(PWD, "afc_lambdas")),
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+
+        create_forecast_step = \
+            tasks.LambdaInvoke(
+                self,
+                "CreateforecastStep",
+                lambda_function=create_forecast_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        create_forecast_step.add_retry(
+            backoff_rate=1.1,
+            interval=core.Duration.seconds(60),
+            max_attempts=2000,
+            errors=["ResourceNotFoundException",
+                    "ResourceInUseException",
+                    "ResourcePendingException"])
+
+        #
+        # CREATE FORECAST EXPORT
+        #
+        create_forecast_export_lambda = \
+            lambda_.Function(
+                self,
+                "CreateExportLambda",
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                handler="index.create_forecast_export_handler",
+                code=lambda_.Code.from_asset(os.path.join(PWD, "afc_lambdas")),
+                environment={
+                    "AFC_ROLE_ARN": afc_role.role_arn
+                },
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+
+        create_forecast_export_step = \
+            tasks.LambdaInvoke(
+                self,
+                "CreateExportStep",
+                lambda_function=create_forecast_export_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        create_forecast_export_step.add_retry(
+            backoff_rate=1.1,
+            interval=core.Duration.seconds(60),
+            max_attempts=2000,
+            errors=["ResourceInUseException",
+                    "ResourcePendingException"])
+
+        #
+        # BACKTEST EXPORT FILE(s)
+        #
+        create_predictor_backtest_export_lambda = \
+            lambda_.Function(
+                self,
+                "CreatePredictorBacktestExportLambda",
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                handler="index.create_predictor_backtest_export_handler",
+                code=lambda_.Code.from_asset(os.path.join(PWD, "afc_lambdas")),
+                environment={
+                    "AFC_ROLE_ARN": afc_role.role_arn
+                },
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+
+        create_predictor_backtest_export_step = \
+            tasks.LambdaInvoke(
+                self,
+                "CreatePredictorBacktestExportStep",
+                lambda_function=create_predictor_backtest_export_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        create_predictor_backtest_export_step.add_retry(
+            backoff_rate=1.1,
+            interval=core.Duration.seconds(60),
+            max_attempts=2000,
+            errors=["ResourceInUseException",
+                    "ResourcePendingException"])
+
+        #
+        # POSTPROCESS FORECAST EXPORT FILE(s)
+        #
+        postprocess_lambda = \
+            lambda_.Function(self, 
+                id=f"{construct_id}-PostProcessLambda",
+                code=lambda_.EcrImageCode.from_asset_image(
+                    directory=os.path.join(PWD, "afc_lambdas", "postprocess")),
+                handler=lambda_.Handler.FROM_IMAGE,
+                runtime=lambda_.Runtime.FROM_IMAGE,
+                function_name=f"{construct_id}-PostProcessLambda",
+                memory_size=10240,
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+            
+        postprocess_step = \
+            tasks.LambdaInvoke(
+                self,
+                "PostProcessStep",
+                lambda_function=postprocess_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        postprocess_step.add_retry(
+            backoff_rate=1.1,
+            interval=core.Duration.seconds(30),
+            max_attempts=2000,
+            errors=["NoFilesFound",
+                    "ResourceInUseException",
+                    "ResourcePendingException"])
+
+        # DELETE AFC RESOURCES
+        delete_afc_resources_lambda = \
+            lambda_.Function(
+                self,
+                "DeleteAfcResourcesLambda",
+                runtime=lambda_.Runtime.PYTHON_3_8,
+                handler="index.delete_afc_resources_handler",
+                code=lambda_.Code.from_asset(os.path.join(PWD, "afc_lambdas")),
+                role=afc_role,
+                timeout=core.Duration.seconds(900))
+
+        delete_afc_resources_step = \
+            tasks.LambdaInvoke(
+                self,
+                "DeleteAfcResourcesStep",
+                lambda_function=delete_afc_resources_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        delete_afc_resources_step.add_retry(
+            backoff_rate=1.1,
+            interval=core.Duration.seconds(60),
+            max_attempts=2000,
+            errors=["ResourceNotFoundException",
+                    "ResourceInUseException",
+                    "ResourcePendingException"])
+
+        #
+        # SNS EMAIL
+        #
+        sns_email_lambda = \
+            lambda_.Function(self, f"{construct_id}-SnsAfcEmailLambda",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            environment={"TOPIC_ARN": topic.topic_arn},
+            code=lambda_.Code.from_inline(SNS_EMAIL_LAMBDA_INLINE),
+            handler="index.lambda_handler",
+            role=afc_role)
+
+        sns_email_step = \
+            tasks.LambdaInvoke(
+                self,
+                "SnsAfcEmailStep",
+                lambda_function=sns_email_lambda,
+                payload=sfn.TaskInput.from_object({
+                    "input": sfn.JsonPath.string_at("$")
+                })
+            )
+
+        #
+        # State machine
+        #
+        definition = prepare_step.next(create_predictor_step) \
+                                 .next(create_forecast_step) \
+                                 .next(create_predictor_backtest_export_step) \
+                                 .next(create_forecast_export_step) \
+                                 .next(postprocess_step) \
+                                 .next(delete_afc_resources_step) \
+                                 .next(sns_email_step)
+
+        state_machine = sfn.StateMachine(self,
+            "SfsSsmAfcStateMachine",
+            state_machine_name=f"{construct_id}-AfcStateMachine",
+            definition=definition,
+            timeout=core.Duration.hours(24))
+
+        ssm_state_machine_param = ssm.StringParameter(self,
+            "SfsSsmAfcStateMachineArn",
+            string_value=state_machine.state_machine_arn,
+            parameter_name="SfsAfcStateMachineArn")
